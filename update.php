78ce5fd3a1e995795ec343b267200da55094b515
BenjaminMassey <benjamin.w.massey@gmail.com>
2018-03-20 02:57:16 -0700

    Accidentally had a php file that I ended up not using

--- update.php
+++
@@ -1,5 +0,0 @@
-
-<?php
-  file_put_contents("C:\Users\benja\Desktop\Development\Code\Web\PixiJS\ENG250\ENG250Project\data.txt", "\n", FILE_APPEND);
-  file_put_contents("C:\Users\benja\Desktop\Development\Code\Web\PixiJS\ENG250\ENG250Project\data.txt", "TEST", FILE_APPEND);
-?>
